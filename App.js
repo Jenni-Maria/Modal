34edf7e581091960eded6e9d3aa15f6fc385af53
t1kyje00@students.oamk.fi <t1kyje00@students.oamk.fi>
2023-09-23 21:52:51 +0300

    Modal with message and close function

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Pressable, Modal } from 'react-native';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function close() {
+    setModalVisible(false);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Modal
+      visible={modalVisible}
+      onRequestClose = {close}
+      >
+        <View style={styles.modal}>
+        <Text>This is modal with a message inside</Text>
+        <Pressable onPress={() => {
+          setModalVisible(false);
+      }}>
+        <Text style={styles.close}>Close</Text>
+      </Pressable>
+      </View>
+    </Modal>
+
+    <Pressable onPress={() => {
+      setModalVisible(true);
+    }}>
+      <Text>Press here to show modal message</Text>
+    </Pressable>
     </View>
   );
 }
@@ -16,5 +38,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
-});
+  modal: {
+    marginTop: 100,
+    padding: 20,
+    backgroundColor: '#fafafa',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#333',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+    },
+    close: {
+      marginTop: 50,
+      color: '#333',
+      fontWeight: 'bold',
+    }
+  }
+);
